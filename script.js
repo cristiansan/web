e84c6aa5054edb5823599d2ba173b6d10b942e5b
Cristian San <cristiansan@gmail.com>
2023-11-24 13:06:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ lightBtn.addEventListener('click', () => {
   body.classList.add('light-theme');
   body.classList.remove('dark-theme');
 });
+
+// Array de textos para los proyectos
+const projectTexts = {
+  currentProject: {
+    title: "Proyecto Actual:",
+    text: "Texto del Proyecto Actual..."
+  },
+  previousProjects: {
+    title: "Proyectos Anteriores:",
+    text: "Texto de Proyectos Anteriores..."
+  }
+};
+
+// Función para cambiar el texto de las secciones
+function changeProjectText(sectionId, newText) {
+  const sectionText = document.getElementById(sectionId + 'Text');
+  sectionText.textContent = newText.text;
+}
+
+// Cambiar el texto al cargar la página
+window.onload = function() {
+  changeProjectText('currentProject', projectTexts.currentProject);
+  changeProjectText('previousProjects', projectTexts.previousProjects);
+};
+
